dac0bceb92c07ae767f74a3ef7cef0919c2c4060
Niklas von Hirschfeld <niklas@vonhirschfeld.eu>
2024-03-10 13:13:27 +0100

    non static in shader

--- assets/shaders/vhs.glsl
+++ assets/shaders/vhs.glsl
@@ -31,6 +31,8 @@ in vec2 fTexCoords;
 in float fTexId;
 uniform float time;
 
+vec4 color;
+
 uniform sampler2D uTextures[8];
 
 out vec4 FragColor;
@@ -42,7 +44,32 @@ float rand(vec2 co){
 void main() {
 	if (fTexId > 0 ) {
 		int id = int(fTexId);
-		vec4 color = fColor * texture(uTextures[id], fTexCoords);
+        switch (id) {
+            case 0:
+                color = fColor * texture(uTextures[0], fTexCoords);
+            break;
+            case 1:
+                color = fColor * texture(uTextures[1], fTexCoords);
+            break;
+            case 2:
+                color = fColor * texture(uTextures[2], fTexCoords);
+            break;
+            case 3:
+                color = fColor * texture(uTextures[3], fTexCoords);
+            break;
+            case 4:
+                color = fColor * texture(uTextures[4], fTexCoords);
+            break;
+            case 5:
+                color = fColor * texture(uTextures[5], fTexCoords);
+            break;
+            case 6:
+                color = fColor * texture(uTextures[6], fTexCoords);
+            break;
+            case 7:
+                color = fColor * texture(uTextures[7], fTexCoords);
+            break;
+		}
 		if (color.a > 0.5) {
 
 			// Adding noise
@@ -50,9 +77,54 @@ void main() {
 
 			// Simulate color distortion and slight position shift
 			vec2 shift = vec2(rand(fTexCoords + time) * 0.005 - 0.0025, 10.0);
-			vec4 rColor = texture(uTextures[id], fTexCoords + shift);
-			vec4 gColor = texture(uTextures[id], fTexCoords);
-			vec4 bColor = texture(uTextures[id], fTexCoords - shift);
+
+			vec4 rColor;
+			vec4 gColor;
+			vec4 bColor; 
+
+
+			switch (id) {
+				case 0:
+					rColor = texture(uTextures[0], fTexCoords + shift);
+					gColor = texture(uTextures[0], fTexCoords);
+					bColor = texture(uTextures[0], fTexCoords - shift);
+				break;
+				case 1:
+					rColor = texture(uTextures[1], fTexCoords + shift);
+					gColor = texture(uTextures[1], fTexCoords);
+					bColor = texture(uTextures[1], fTexCoords - shift);
+				break;
+				case 2:
+					rColor = texture(uTextures[2], fTexCoords + shift);
+					gColor = texture(uTextures[2], fTexCoords);
+					bColor = texture(uTextures[2], fTexCoords - shift);
+				break;
+				case 3:
+					rColor = texture(uTextures[3], fTexCoords + shift);
+					gColor = texture(uTextures[3], fTexCoords);
+					bColor = texture(uTextures[3], fTexCoords - shift);
+				break;
+				case 4:
+					rColor = texture(uTextures[4], fTexCoords + shift);
+					gColor = texture(uTextures[4], fTexCoords);
+					bColor = texture(uTextures[4], fTexCoords - shift);
+				break;
+				case 5:
+					rColor = texture(uTextures[5], fTexCoords + shift);
+					gColor = texture(uTextures[5], fTexCoords);
+					bColor = texture(uTextures[5], fTexCoords - shift);
+				break;
+				case 6:
+					rColor = texture(uTextures[6], fTexCoords + shift);
+					gColor = texture(uTextures[6], fTexCoords);
+					bColor = texture(uTextures[6], fTexCoords - shift);
+				break;
+				case 7:
+					rColor = texture(uTextures[7], fTexCoords + shift);
+					gColor = texture(uTextures[7], fTexCoords);
+					bColor = texture(uTextures[7], fTexCoords - shift);
+				break;
+			}
 
 			// Combine color channels with noise
 			color.rgb = vec3(rColor.r, gColor.g, bColor.b) + vec3(noise, noise, noise);
